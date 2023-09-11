72acba6ebb1bf599ea270bf20d4e4a6f3851d932
eskayML <kalusam619@gmail.com>
2023-09-11 12:02:08 +0100

    fix: timestamp

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function updateUTCTime() {
   const utcTimeElement = document.getElementById('utc-time');
+  const timstamp = document.getElementById('timestamp')
 
   function updateTime() {
-    const currentTimeMillis = new Date().getTime();
-    const currentTimeUTC = new Date(currentTimeMillis).toUTCString();
-    utcTimeElement.innerHTML = currentTimeUTC;
+    const currentTimeMillis = new Date().getTime() / 1000; // Convert to seconds
+    const currentDateTime = new Date(currentTimeMillis * 1000); // Convert back to milliseconds
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayOfWeek = daysOfWeek[currentDateTime.getUTCDay()];
+
+    utcTimeElement.innerHTML = dayOfWeek;
+    timestamp.innerHTML= new Date().getTime()
   }
 
   updateTime();
